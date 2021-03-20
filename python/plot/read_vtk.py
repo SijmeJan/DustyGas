e0cd7f1b8e6629a5b430f319c0c885b3d938793b
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-03-20 23:44:21 +0000

    add plotting

---
+++ python/plot/read_vtk.py
@@ -0,0 +1,44 @@
+import numpy as np
+from vtk import vtkUnstructuredGridReader, vtkCellCenters
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+from matplotlib import pyplot as plt
+
+class SnapShot():
+    def __init__(self, filename):
+        reader = vtkUnstructuredGridReader()
+        reader.SetFileName(filename)
+        reader.ReadAllVectorsOn()
+        reader.ReadAllScalarsOn()
+        reader.Update()
+
+        output = reader.GetOutput()
+
+        # State vector
+        self.Q = vtk_to_numpy(output.GetCellData().GetArray(0))
+
+        # Cell centre coordinates
+        x = np.zeros((np.shape(self.Q)[0],3)) - 1
+        temp = numpy_to_vtk(x)
+        vtkCellCenters().ComputeCellCenters(output, temp)
+        self.x = vtk_to_numpy(temp)
+
+filename = '../../data/state-100-rank-0.vtk'
+
+#s = SnapShot(filename)
+#plt.tricontourf(s.x[:,0], s.x[:,1], s.Q[:,4])
+
+e = np.zeros((101))
+
+for i in range(0, 101):
+    s = SnapShot('../../data/state-{}-rank-0.vtk'.format(i))
+    #e[i] = np.sum(s.Q[:,6]*s.Q[:,6]/s.Q[:,4])
+    e[i] = np.sum(np.abs(s.Q[:,6]/s.Q[:,4]))
+
+#plt.xscale('log')
+plt.yscale('log')
+plt.plot(e)
+
+plt.plot(0.1*np.exp(0.21*np.arange(0,101)))
+
+
+plt.show()
