92ff0a830faf477c3a0432f20d0dd96a3d088410
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-03-21 20:46:59 +0000

    initial

--- python/write_cpp/initial.py
+++ python/write_cpp/initial.py
@@ -7,15 +7,15 @@ def write_initial(lines, n_dust, mu, Stokes):
     denom = '((1 + {})*(1 + {}) + {}*{})'.format(mu, mu, tau, tau)
 
     initial = ['  if (t == 0.0) {\n',
-               '    Q[0] = 1.0;\n',
-               '    Q[1] = 2*{}*{}/{};\n'.format(mu, tau, denom),
-               '    Q[2] = 0.0;\n',
-               '    Q[3] = -(1 + {}*{}*{}/{})/(1 + {});\n'.format(mu, tau, tau, denom, mu)]
+               '    Q[0] = 1.0*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n',
+               '    Q[1] = 2*{}*{}/{}*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n'.format(mu, tau, denom),
+               '    Q[2] = 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30);\n',
+               '    Q[3] = -(1 + {}*{}*{}/{})/(1 + {})*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n'.format(mu, tau, tau, denom, mu)]
     for n in range(0, n_dust):
-        initial.extend(['    Q[{}] = {}*(1.0 + 0.001*cos(x[0]*30 + x[1]*30));\n'.format(4*n + 4, mu),
-                        '    Q[{}] = -2*{}*{}/{};\n'.format(4*n + 5, mu, tau, denom),
-                        '    Q[{}] = 0.0;\n'.format(4*n + 6),
-                        '    Q[{}] = -{}*(1-{}*{}/{})/(1+{});\n'.format(4*n + 7, mu, tau, tau, denom, mu)])
+        initial.extend(['    Q[{}] = {}*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n'.format(4*n + 4, mu),
+                        '    Q[{}] = -2*{}*{}/{}*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n'.format(4*n + 5, mu, tau, denom),
+                        '    Q[{}] = 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30);\n'.format(4*n + 6),
+                        '    Q[{}] = -{}*(1-{}*{}/{})/(1+{})*(1.0 + 0.001*cos(x[0]*30 + x[1]*30) +  0.001*sin(x[0]*30 + x[1]*30));\n'.format(4*n + 7, mu, tau, tau, denom, mu)])
     initial.extend(['  }\n'])
 
     for i in range(0, len(lines)):
