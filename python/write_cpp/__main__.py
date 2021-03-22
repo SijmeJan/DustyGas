158fc8d3e500ba937d7cc2e6451956b12cb63916
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-03-22 12:36:58 +0000

    initial

--- python/write_cpp/__main__.py
+++ python/write_cpp/__main__.py
@@ -75,7 +75,7 @@ if (n_vars % 4 != 0):
     exit(1)
 
 # Parameters
-c = 20.0          # gas sound speed
+c = 2.0          # gas sound speed
 q = 1.5           # non-dimensional shear rate
 Stokes = [0.1]    # list of Stokes numbers
 mu = 3.0          # dust/gas ratio
