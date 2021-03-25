4ac12e2da0edc9fb6e7f9a3d9c2ec45d5f77e522
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-03-25 20:06:22 +0000

    ader-dg

--- python/plot/read_vtk.py
+++ python/plot/read_vtk.py
@@ -31,9 +31,9 @@ class SnapShot():
         self.Q = self.Q[sel]
         self.x = self.x[sel]
 
-filename = '../../data/state-0-rank-0.vtk'
+filename = '../../data/state-3-rank-0.vtk'
 s = SnapShot(filename)
-s.remove_ghost(1)
+#s.remove_ghost(3)
 
 #cs = plt.tricontourf(s.x[:,0], s.x[:,1], s.Q[:,0], 100)
 #plt.colorbar(cs)
