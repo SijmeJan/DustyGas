8b1da1f27b525e484bd93540727dde10ab3dea35
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-04-20 14:45:57 +0100

    eta=0

--- python/plot/read_vtk.py
+++ python/plot/read_vtk.py
@@ -31,16 +31,16 @@ class SnapShot():
         self.Q = self.Q[sel]
         self.x = self.x[sel]
 
-filename = '../../data/state-1.vtk'
-s = SnapShot(filename)
+#filename = '../../data/state-10.vtk'
+#s = SnapShot(filename)
 #s.remove_ghost(10)
 
-cs = plt.tricontourf(s.x[:,0], s.x[:,1], s.Q[:,1], 100)
-plt.colorbar(cs)
+#cs = plt.tricontourf(s.x[:,0], s.x[:,1], s.Q[:,1], 100)
+#plt.colorbar(cs)
 
-plt.show()
+#plt.show()
 
-exit(0)
+#exit(0)
 
 #sel = np.asarray(s.x[:,1] == np.min(s.x[:,1])).nonzero()
 #plt.plot(s.x[sel,0], s.Q[sel,1], marker='o', linestyle='None', color='blue')
@@ -51,7 +51,7 @@ exit(0)
 #plt.show()
 #exit(0)
 
-n = 181
+n = 200
 e = np.zeros((n))
 #direcs = ['../../data/order2', '../../data/order3']
 direcs = ['../../data']
@@ -64,7 +64,7 @@ plt.xlabel(r'$\Omega t$')
 
 for direc in direcs:
     for i in range(0, n):
-        s = SnapShot(direc + '/state-{}.vtk'.format(i))
+        s = SnapShot(direc + '/state-{}-rank-0.vtk'.format(i))
         #e[i] = np.sum(s.Q[:,6]*s.Q[:,6]/s.Q[:,4])/len(s.Q[:,4])
         e[i] = np.mean(s.Q[:,1]/s.Q[:,0])
 
