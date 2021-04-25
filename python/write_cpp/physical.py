0bcf5d22150dc7c7fefc17bc7f9968083bcd394f
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-04-25 22:10:38 +0100

    physically admissible

--- python/write_cpp/physical.py
+++ python/write_cpp/physical.py
@@ -12,12 +12,12 @@ def write_physical(output_dir, solver_name, n_dust):
     body = ['  // Check if all quantities are finite\n']
 
     for i in range(0, 4 + 4*n_dust):
-        body.append('  if (!std::isfinite(localObservablesMin[{}])) return false;\n'.format(i))
-        body.append('  if (!std::isfinite(localObservablesMax[{}])) return false;\n'.format(i))
+        body.append('  if (!std::isfinite(localDMPObservablesMin[{}])) return false;\n'.format(i))
+        body.append('  if (!std::isfinite(localDMPObservablesMax[{}])) return false;\n'.format(i))
 
     body.append('\n  // Check for positive densities\n')
     for i in range(0, n_dust + 1):
-        body.append('  if (localObservablesMin[{}] <= 0.0) return false;\n'.format(4*i))
+        body.append('  if (localDMPObservablesMin[{}] <= 0.0) return false;\n'.format(4*i))
 
     body.append('  return true;\n')
 
